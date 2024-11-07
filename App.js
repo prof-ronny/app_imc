9483135f453f8c1561c4971778b4d5fb4ab8f70e
Carlos Ronny de Sousa <135059087+prof-ronny@users.noreply.github.com>
2024-11-07 23:30:30 +0000

    Criação do projeto e Caixa de Texto de Peso

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Aplicativo Calculo IMC</Text>
+      <TextInput placeholder="Peso" keyboardType="numeric" />
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
